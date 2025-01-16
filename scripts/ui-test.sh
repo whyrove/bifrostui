15bb1f96529eec48f6bb63e41e0f1037e1ceb1fc
whyrove <whyrove@163.com>
2025-01-16 14:41:54 +0800

    feat: 测试看看

--- scripts/ui-test.sh
+++ scripts/ui-test.sh
@@ -1,5 +1,14 @@
 # 判断是否已经有reference
-if [ ! -d "backstop_data/bitmaps_reference" ];then
-  backstop reference
-  else backstop test
-fi
+while :
+  do
+    if lsof -i:8000; then
+      if [ ! -d "backstop_data/bitmaps_reference" ];then
+       backstop reference
+       break;
+      else
+        backstop test
+        break;
+      fi
+    fi
+  sleep 3
+done
