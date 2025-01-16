bd94c0f793d301f6371af193a4917530c95c4668
whyrove <whyrove@163.com>
2025-01-16 16:01:41 +0800

    feat: 测试看看

--- scripts/ui-test.sh
+++ scripts/ui-test.sh
@@ -4,9 +4,11 @@ while :
     if lsof -i:8000; then
       if [ ! -d "backstop_data/bitmaps_reference" ];then
        backstop reference
+       kill -9 $(lsof -i :8000 | awk '{print $2}')
        break;
       else
         backstop test
+        kill -9 $(lsof -i :8000 | awk '{print $2}')
         break;
       fi
     fi
